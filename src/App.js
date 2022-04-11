d9855ac98795c925bc6ed64039f9108cf379a629
Ruslan.Shayakhmetov <ruslan.shayakhmetov@edetek.com>
2022-04-11 20:35:32 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import HomePage from './homepage.template';
+
+const App = () => {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
